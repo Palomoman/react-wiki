d2436f70bcf67be3a21d5baf011ce6336577a45f
Palomoman <guillemcm94@gmail.com>
2022-06-01 11:14:59 +0200

    Estructura hecha. datos fecheados bien. Paginacion correcta

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Cards = ({ results }) => {
+
+    let display
+
+    if (results) {
+        display = results.map(x => {
+            let { id, name } = x
+            return (
+                <div key={id} className="col-4">{name}</div>
+            )
+        })
+    }
+    else {
+        display = "No characters found"
+    }
+    return (
+        <>{display}</>
+    )
+}
+
+export default Cards
